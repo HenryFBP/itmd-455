7d07561fd3370ba6299d2fa74dbb59f52c8bf44a
Henry Post <HenryFBP@gmail.com>
2018-08-21 16:44:36 -0500

    add event listener for button that will appendThing()

--- TestApplication/app/src/main/java/me/henryfbp/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/me/henryfbp/testapplication/MainActivity.java
@@ -1,12 +1,31 @@
 package me.henryfbp.testapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    LinearLayout linearLayout;
+
+    public void appendThing(View v) {
+        Log.i("test", "button clicked!");
+
+        EditText text = new EditText(this);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        button = findViewById(R.id.button);
+        linearLayout = findViewById(R.id.linearLayout);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
