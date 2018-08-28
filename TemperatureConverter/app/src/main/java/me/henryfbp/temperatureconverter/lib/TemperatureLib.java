1cde4b7e74b585ba752aab358fd7dc0d70ab63cf
Henry Post <HenryFBP@gmail.com>
2018-08-28 17:54:57 -0500

    add mxparser for cool math reasons

--- TemperatureConverter/app/src/main/java/me/henryfbp/temperatureconverter/lib/TemperatureLib.java
+++ TemperatureConverter/app/src/main/java/me/henryfbp/temperatureconverter/lib/TemperatureLib.java
@@ -1,13 +1,21 @@
 package me.henryfbp.temperatureconverter.lib;
 
+import org.mariuszgromada.math.mxparser.Expression;
+
 public final class TemperatureLib {
-    public static final double celsiusToFahrenheit(double c)
-    {
-        return (c * (9d / 5d)) + 32d;
+
+    public static final String celsius_to_fahrenheit = "y = ((9/5) * x) + 32";
+
+    public static double celsiusToFahrenheit(double x) {
+
+        Expression e = new Expression(celsius_to_fahrenheit);
+
+        e.setArgumentValue("x", x);
+
+        return e.calculate();
     }
 
-    public static final double fahrenheitToCelsius(double f)
-    {
+    public static double fahrenheitToCelsius(double f) {
         return (f - 32d) * (5d / 9d);
     }
 }
