82f6faf288643753ec095fc2fe86503adc6038d8
Henry Post <HenryFBP@gmail.com>
2018-09-24 15:08:06 -0500

    add lab 3 skeleton

---
+++ labs/3/Lab3/app/src/main/java/me/henryfbp/quotes/QuoteReaderActivity.kt
@@ -0,0 +1,12 @@
+package me.henryfbp.quotes
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QuoteReaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote_reader)
+    }
+}
---
+++ labs/3/Lab3/app/src/test/java/me/henryfbp/quotes/ExampleUnitTest.kt
@@ -0,0 +1,33 @@
+package me.henryfbp.quotes
+
+import android.graphics.Color
+import me.henryfbp.library.HLib.mixColors
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+            /***
+             * Can we import my crappy functions?
+             */
+    fun can_import_henrylib() {
+        try {
+
+            var c = mixColors(Color.BLACK, Color.WHITE, 1.0f)
+        } catch (e: RuntimeException) {
+            assertEquals(e.javaClass, RuntimeException().javaClass)
+        }
+
+
+    }
+}
