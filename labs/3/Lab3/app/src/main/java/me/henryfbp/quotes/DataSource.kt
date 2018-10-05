55b8dce9d20e4a3d50455983f7fe0ceb74c32296
Henry Post <HenryFBP@gmail.com>
2018-10-05 18:11:58 -0500

    add my own quotes

--- labs/3/Lab3/app/src/main/java/me/henryfbp/quotes/DataSource.kt
+++ labs/3/Lab3/app/src/main/java/me/henryfbp/quotes/DataSource.kt
@@ -10,16 +10,23 @@ class DataSource {
     constructor() {
 
         Collections.addAll(imageQuotePool,
-                ImageQuote(R.drawable.steve_1, R.drawable.steve_hd_1, R.string.quote_1),
-                ImageQuote(R.drawable.steve_2, R.drawable.steve_hd_2, R.string.quote_2),
-                ImageQuote(R.drawable.steve_3, R.drawable.steve_hd_3, R.string.quote_3),
-                ImageQuote(R.drawable.steve_4, R.drawable.steve_hd_4, R.string.quote_4),
-                ImageQuote(R.drawable.steve_5, R.drawable.steve_hd_5, R.string.quote_5),
-                ImageQuote(R.drawable.steve_6, R.drawable.steve_hd_6, R.string.quote_6),
-                ImageQuote(R.drawable.steve_7, R.drawable.steve_hd_7, R.string.quote_7),
-                ImageQuote(R.drawable.steve_8, R.drawable.steve_hd_8, R.string.quote_8),
-                ImageQuote(R.drawable.steve_9, R.drawable.steve_hd_9, R.string.quote_9),
-                ImageQuote(R.drawable.steve_10, R.drawable.apple_hd, R.string.quote_10))
+                ImageQuote(R.drawable.steve_1, R.drawable.steve_hd_1, R.string.quote_1_sj),
+                ImageQuote(R.drawable.steve_2, R.drawable.steve_hd_2, R.string.quote_2_sj),
+                ImageQuote(R.drawable.steve_3, R.drawable.steve_hd_3, R.string.quote_3_sj),
+                ImageQuote(R.drawable.steve_4, R.drawable.steve_hd_4, R.string.quote_4_sj),
+                ImageQuote(R.drawable.steve_5, R.drawable.steve_hd_5, R.string.quote_5_sj),
+                ImageQuote(R.drawable.steve_6, R.drawable.steve_hd_6, R.string.quote_6_sj),
+                ImageQuote(R.drawable.steve_7, R.drawable.steve_hd_7, R.string.quote_7_sj),
+                ImageQuote(R.drawable.steve_8, R.drawable.steve_hd_8, R.string.quote_8_sj),
+                ImageQuote(R.drawable.steve_9, R.drawable.steve_hd_9, R.string.quote_9_sj),
+                ImageQuote(R.drawable.steve_10, R.drawable.apple_hd, R.string.quote_10_sj),
+
+                ImageQuote(R.drawable.johnny_1, R.drawable.johnny_hd_1, R.string.quote_1_j),
+                ImageQuote(R.drawable.arnold_1, R.drawable.arnold_hd_1, R.string.quote_1_a),
+                ImageQuote(R.drawable.brooke_1, R.drawable.brooke_hd_1, R.string.quote_1_bs),
+                ImageQuote(R.drawable.dan_1, R.drawable.dan_hd_1, R.string.quote_1_dq),
+                ImageQuote(R.drawable.george_1, R.drawable.george_hd_1, R.string.quote_1_gg)
+        )
         Log.i("test", imageQuotePool.size.toString())
     }
 
